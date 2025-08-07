1f090d680547297c6b7e8b9f618168504c40b1cd
Dilsanka Sajith <Dilsanka39@gmail.com>
2025-08-08 00:02:18 +0530

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/sample-data';
+
+export const metadata = {
+  title: 'Home',
+};
+
+const Homepage = () => {
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
+};
+
+export default Homepage;
---
+++ app/loading.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import loader from '@/assets/loader.gif';
+
+const LoadingPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        width: '100vw',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Image src={loader} height={150} width={150} alt="Loading..." />
+    </div>
+  );
+};
+
+export default LoadingPage;
